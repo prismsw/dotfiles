a3f008a3b6d83fd300f6815a13e121d647ffa4a8
Simon Strassl <simon.strassl@gmail.com>
2012-04-09 16:06:25 +0200

    Added applications menu.

--- rc.lua
+++ rc.lua
@@ -80,16 +80,24 @@ end
 -- {{{ Menu
 -- Create a laucher widget and a main menu
 myawesomemenu = {
-   { "manual", terminal .. " -e man awesome" },
-   { "edit config", editor_cmd .. " " .. awesome.conffile },
-   { "restart", awesome.restart },
-   { "quit", awesome.quit }
+	{ "manual", terminal .. " -e man awesome" },
+	{ "edit config", editor_cmd .. " " .. awesome.conffile },
+	{ "restart", awesome.restart },
+	{ "quit", awesome.quit }
 }
 
-mymainmenu = awful.menu({ items = { { "awesome", myawesomemenu, beautiful.awesome_icon },
-                                    { "open terminal", terminal }
-                                  }
-                        })
+applicationsmenu = {
+	{ "Chrome", "google-chrome" },
+	{ "Chromium", "chromium" },
+	{ "Gimp", "gimp"},
+	{ "VLC", "vlc"}
+}
+
+mymainmenu = awful.menu({ items = { 
+		{ "awesome", myawesomemenu, beautiful.awesome_icon },
+		{ "applications", applicationsmenu },
+		{ "open terminal", terminal }
+	}})
 
 mylauncher = awful.widget.launcher({ image = image(beautiful.awesome_icon),
                                      menu = mymainmenu })
